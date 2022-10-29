9159350c9443b17e64db4ce5bc2b629f6d9e0c47
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-10-29 16:11:02 +0300

    Initial commit

---
+++ core-service/src/test/java/ru/gb/coreservice/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.coreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
