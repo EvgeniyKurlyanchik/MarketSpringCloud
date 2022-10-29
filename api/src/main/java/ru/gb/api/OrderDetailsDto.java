abde00e3a50873b671640f5de46cd720c864fc90
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-10-29 18:07:12 +0300

    Собрал структуру микросервисного приложения.

---
+++ api/src/main/java/ru/gb/api/OrderDetailsDto.java
@@ -0,0 +1,11 @@
+package ru.gb.api;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OrderDetailsDto {
+    private String address;
+    private String phone;
+}
