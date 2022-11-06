74ba735a54ec7e160965a7d289142e3be6d225c3
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-11-06 17:08:22 +0300

    Исправил ошибки , миграция DB заработала. Есть авторизация , добавление продуктов  в корзину.

---
+++ cart-service/src/main/java/ru/gb/cartservice/controllers/CartController.java
@@ -0,0 +1,27 @@
+package ru.gb.cartservice.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.api.CartDto;
+import ru.gb.cartservice.convertors.CartConverter;
+import ru.gb.cartservice.services.CartService;
+
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class CartController {
+    private final CartService cartService;
+    private final CartConverter cartConverter;
+
+    @GetMapping
+    public CartDto getCurrentCart() {
+        return cartConverter.entityToDto(cartService.getCurrentCart());
+    }
+
+    @GetMapping("/add/{productId}")
+    public void addProductToCart(@PathVariable Long productId) {
+        cartService.addToCart(productId);
+    }
+}
