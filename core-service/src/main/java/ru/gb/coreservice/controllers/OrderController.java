ee847446f687046df57db93a23c9db057b172d25
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-12-01 23:27:13 +0300

    Docker, Swagger.

--- core-service/src/main/java/ru/gb/coreservice/controllers/OrderController.java
+++ core-service/src/main/java/ru/gb/coreservice/controllers/OrderController.java
@@ -1,5 +1,7 @@
 package ru.gb.coreservice.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +16,17 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
+@Tag(name = "Заказы", description = "Методы работы с заказами")
 public class OrderController {
     private final OrderService orderService;
     private final OrderConverter orderConverter;
+    @Operation (summary = "создание нового заказа")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createNewOrder(@RequestHeader String username) {
         orderService.createNewOrder(username);
     }
-
+@Operation(summary = "получение  заказа пользователя")
     @GetMapping
     public List<OrderDto> getUserOrders(@RequestHeader String username) {
         return orderService.findUserOrders(username).stream().map(orderConverter::entityToDto).collect(Collectors.toList());
